279504bc6bae98d3a84562417410647028a339f0
이준영 <tera199810@gmail.com>
2025-06-05 11:28:25 +0900

    refactor : @Valid 예외 내용 수정 검증 메세지 사용하도록 변경

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -5,12 +5,14 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -36,7 +38,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        return getErrorResponse(status, ex.getMessage());
+        Optional<FieldError> fieldError = Optional.ofNullable(ex.getBindingResult().getFieldError());
+        String message = "Bad Request";
+
+        if (fieldError.isPresent()) {
+            message = fieldError.get().getDefaultMessage();
+        }
+
+        return getErrorResponse(status, message);
     }
 
     private ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
