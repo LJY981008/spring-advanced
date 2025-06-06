5d7a710866f172ed5a9359cda8f879ffb919fd28
이준영 <tera199810@gmail.com>
2025-06-06 15:08:50 +0900

    feat : void 반환 타입 리스폰서 생성 ResponseEntity<Void> 타입 리스폰서 추가

--- src/main/java/org/example/expert/config/EntityResponser.java
+++ src/main/java/org/example/expert/config/EntityResponser.java
@@ -8,4 +8,8 @@ public class EntityResponser {
     public static <T> ResponseEntity<T> responser(T entity, HttpStatusCode status){
         return new ResponseEntity<>(entity, status);
     }
+
+    public static ResponseEntity<Void> voidResponser(HttpStatusCode status){
+        return ResponseEntity.status(status).build();
+    }
 }
