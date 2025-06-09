39bc360d5dc5034b6d7d5f897ccb9e9bc114600b
이준영 <tera199810@gmail.com>
2025-06-09 13:29:11 +0900

    feat : CommentService 테스트 코드 작성 라인 커버 100%

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.comment.service;
 
 import org.example.expert.domain.comment.dto.request.CommentSaveRequest;
+import org.example.expert.domain.comment.dto.response.CommentResponse;
 import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
@@ -9,23 +10,32 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
+import org.example.expert.domain.user.dto.response.UserResponse;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CommentServiceTest {
+
+    @Mock
+    Comment comment;
 
     @Mock
     private CommentRepository commentRepository;
@@ -70,5 +80,38 @@ class CommentServiceTest {
 
         // then
         assertNotNull(result);
+    }
+
+    @Test
+    @DisplayName("댓글 조회 성공 시 전체 댓글을 반환합니다.")
+    void getComments_success() {
+        //given
+        long todoId = 1L;
+
+        List<Comment> commentList = new ArrayList<>(List.of(comment, comment, comment));
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+
+        when(commentRepository.findByTodoIdWithUser(anyLong())).thenReturn(commentList);
+        when(comment.getUser()).thenReturn(User.fromAuthUser(authUser));
+        when(comment.getId()).thenReturn(todoId);
+        when(comment.getContents()).thenReturn("contents");
+
+        //when
+        List<CommentResponse> comments = commentService.getComments(todoId);
+
+        //then
+        comments.forEach(commentResponse ->
+                assertThat(commentResponse.getContents()).isEqualTo("contents")
+        );
+        comments.forEach(commentResponse ->
+                assertThat(commentResponse.getId()).isEqualTo(todoId)
+        );
+        comments.forEach(commentResponse ->
+                assertThat(commentResponse.getUser().getId()).isEqualTo(authUser.getId())
+        );
+        comments.forEach(commentResponse ->
+                assertThat(commentResponse.getUser().getEmail()).isEqualTo(authUser.getEmail())
+        );
+
     }
 }
